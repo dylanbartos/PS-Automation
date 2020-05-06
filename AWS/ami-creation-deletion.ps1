c33f5fa7791c83aa8993b183b47b5d0fdd66be73
Dylan Bartos <mrdboutdoors@gmail.com>
2020-05-06 12:25:17 -0700

    Simplified and updated for Lambda environment

--- AWS/ami-creation-deletion.ps1
+++ AWS/ami-creation-deletion.ps1
@@ -1,9 +1,14 @@
-Import-Module AWSPowerShell.NetCore
+#Requires -Modules @{ModuleName='AWSPowerShell.NetCore';ModuleVersion='3.3.590.0'}
 
 #Specify Environment Variables#
-$EC2Tag = "Auto-Ami" #The Boolean tag used to enable/disable ami creation/deletion
-$Debug = "True" #Logs expected ouput rather than executing any action
-$AMIsToKeep = 7 #Stores X number of AMIs
+#$EC2Tag = "Auto-Ami" #The Boolean tag used to enable/disable ami creation/deletion
+#$Debug = "False" #Logs expected ouput rather than executing any action
+#$AMIsToKeep = 7 #Stores X number of AMIs
+
+#Collect Environment Variables from Lambda#
+$EC2Tag = $env:EC2Tag
+$Debug = $env:Debug
+$AMIsToKeep = $env:AMIsToKeep -as [int]
 
 #Pulls Tags and Instances from AWS
 $Instances = Get-Ec2Tag -Filter @{Name="resource-type";Value="instance"}, @{Name="key";Value="Name"}
@@ -11,10 +16,7 @@ $NamedInstances = $Instances | Select-Object @{Name="InstanceID";Expression={$PS
 $Instances = Get-Ec2Tag -Filter @{Name="resource-type";Value="instance"}, @{Name="key";Value=$EC2Tag} 
 $Instances = $Instances | Select-Object @{Name="InstanceID";Expression={$PSItem.ResourceID}}, @{Name="AutoAmi";Expression={$PSItem.Value}} | Where-Object {$_.AutoAmi.ToUpper() -eq "TRUE"}
 
-#Pulls volume IDs
-$Volumes = $Instances | ForEach-Object {Get-EC2Volume -Filter @{Name="attachment.instance-id"; Values="$($Instances.InstanceID)"}}
-
-#Joins key/value pairs into the Instance object
+#Joins key/value pairs into the Instances object
 $Instances | ForEach-Object {
     #Adds Name tag
     For ($i=0; $i -le ($NamedInstances.PSObject.Properties.Value.Name.Count - 1); $i++){
@@ -22,23 +24,12 @@ $Instances | ForEach-Object {
             $_ | Add-Member -MemberType NoteProperty -Name "Name" -Value $NamedInstances[$i].Name
         }
     }
-
-    #Adds volumes
-    For ($i=0; $i -le ($volumes.count - 1); $i++){
-        If ($Volumes[$i].Attachment.InstanceID -eq $_.InstanceID){
-            If ($null -eq $_.Volumes){
-                $_ | Add-Member -MemberType NoteProperty -Name "Volumes" -Value @($Volumes[$i].Attachment.VolumeID)
-            } ElseIf ($null -ne $_.Volumes){
-                $_.Volumes += "$($Volumes[$i].Attachment.VolumeID)" 
-            }
-        }
-    }
 }
 
 #Main Execution
 If ($Debug -eq $True){
     Write-Host "Instances set to AMI:"
-    $Instances | ForEach-Object {Write-Host $_.InstanceID}
+    $Instances | ForEach-Object {Write-Host "$($_.InstanceID) = $($_.Name)"}
 
     Write-Host "AMIs set to unregister/snapshots set to delete:"
     $Instances | ForEach-Object {
@@ -47,36 +38,27 @@ If ($Debug -eq $True){
         #Remove AMIs older than 7 days
         For ($i=$AMIsToKeep; $i -le ($AMIs.Count - 1); $i++){
             Write-Host $AMIs[$i].ImageId
-            For ($j=0; $j -le ($Instances.Volumes.Count - 1); $j++){
-                #Pull all snapshot for each volume
-                $Snapshots = Get-EC2Snapshot -Filter @{Name="volume-id"; Values="$($_.Volumes[$j])"}
-                $InstancesTmp = $_ 
-                #Filter snapshots by the specific AMI where is being unregistereed
-                $Snapshots = $Snapshots | Where-Object {$_.Description -eq "Created by CreateImage($($InstancesTmp.InstanceID)) for $($AMIs[$i].ImageId) from $($InstancesTmp.Volumes[$j])"}
-                Write-Host "$($Snapshots.SnapshotId)"
-            }
+            $AMIs[$i].BlockDeviceMapping.EBS.SnapshotId
         }
     }
-} ElseIf ($Debug -ne $True){
+} ElseIf ($Debug -eq $False){
     $Instances | ForEach-Object {
-        New-EC2Image -InstanceID $_.InstanceID -Name "$(Get-Date -Format 'yyyyMMdd')_$($_.Name)_AutoAmi-$($(New-Guid).ToString().SubString(0,5))" -Description "Automatic AMI created for $($_.InstanceID)" -NoReboot $True | Out-Null
-        
+        #Create new AMI and tag
+        $Guid = $(New-Guid).ToString().SubString(0,5)
+        $NewAMI = New-EC2Image -InstanceID $_.InstanceID -Name "$(Get-Date -Format 'yyyyMMdd')_$($_.Name)_AutoAmi-$Guid" -Description "Automatic AMI created for $($_.InstanceID)" -NoReboot $True
+        $NewTag = New-Object Amazon.EC2.Model.Tag
+        $NewTag.Key = "Name"
+        $NewTag.Value = "$($_.Name)_AutoAmi-$Guid"
+        New-EC2Tag -Resource $NewAMI -Tag $NewTag
+
         $AMIs = Get-EC2Image -Filters @{Name="description"; Values="Automatic AMI created for $($_.InstanceID)"} | Sort-Object -Property CreationDate -Descending
+        #Tag snapshots
+        $AMIs[0].BlockDeviceMapping.EBS.SnapshotId | ForEach-Object {New-EC2Tag -Resource $_ -Tag $NewTag}
 
         #Remove AMIs older than 7 days
         For ($i=$AMIsToKeep; $i -le ($AMIs.Count - 1); $i++){
-            #Grab Snapshots before AMI is unregistered
-            $SnapshotIDsToRemove = @()
-            For ($j=0; $j -le ($Instances.Volumes.Count - 1); $j++){
-                #Pull all snapshots
-                $Snapshots = Get-EC2Snapshot -Filter @{Name="volume-id"; Values="$($_.Volumes[$j])"}
-                $InstancesTmp = $_ 
-                #Filter snapshots to just those that match the AMI    
-                $Snapshots = $Snapshots | Where-Object {$_.Description -eq "Created by CreateImage($($InstancesTmp.InstanceID)) for $($AMIs[$i].ImageId) from $($InstancesTmp.Volumes[$j])"}
-                $Snapshots | ForEach-Object {$SnapshotIDsToRemove += $_.SnapShotId}
-            }
             Unregister-EC2Image -ImageID $AMIs[$i].ImageId
-            $SnapshotIDsToRemove | ForEach-Object {Remove-EC2Snapshot -SnapshotId $_ -Confirm:$false}
+            $AMIs[$i].BlockDeviceMapping.EBS.SnapshotID | ForEach-Object {Remove-EC2Snapshot -SnapshotId $_ -Confirm:$false}
         }
     }
 }
